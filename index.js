11913b89a0f175e1e141d4ef503b17bb0325613e
Rahul Chowdhury <nantukchy@gmail.com>
2021-11-19 20:25:27 +0600

    final

--- index.js
+++ index.js
@@ -142,26 +142,7 @@ async function run(){
             res.json(result);
         });
 
-        // app.put('/users/admin', verifyToken, async (req, res) => {
-        //     const user = req.body;
-        //     const requester = req.decodedEmail;
-        //     if (requester) {
-        //         const requesterAccount = await usersCollection.findOne({ email: requester });
-        //         if (requesterAccount.role === 'admin') {
-        //             const filter = { email: user.email };
-        //             const updateDoc = { $set: { role: 'admin' } };
-        //             const result = await usersCollection.updateOne(filter, updateDoc);
-        //             res.json(result);
-        //         }
-        //     }
-        //     else {
-        //         res.status(403).json({ message: 'you do not have access to make admin' })
-        //     }
-
-        // })
-
-
-
+   
 
 	}
 	finally{
